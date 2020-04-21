8db09badc2181b775a24f596d0de9c5b2afdff13
Mate Iszlai <iszimate@gmail.com>
2020-04-21 14:27:40 +0200

    Add signalR to services

--- CarcassTwwo/CarcassTwwo/Startup.cs
+++ CarcassTwwo/CarcassTwwo/Startup.cs
@@ -26,6 +26,7 @@ namespace CarcassTwwo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
