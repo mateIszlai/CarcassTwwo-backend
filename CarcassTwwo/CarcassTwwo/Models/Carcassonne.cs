68f951e616b24d1de01e4b2e857db9bcb2975d3b
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-22 17:41:58 +0200

    Add properties and methods to Carcassonne

--- CarcassTwwo/CarcassTwwo/Models/Carcassonne.cs
+++ CarcassTwwo/CarcassTwwo/Models/Carcassonne.cs
@@ -7,5 +7,33 @@ namespace CarcassTwwo.Models
 {
     public class Carcassonne
     {
+        public bool IsOver { get; set; }
+        public int RoundsLeft { get; set; }
+        public Client Player1 { get; set; }
+        public Client Player2 { get; set; }
+        public Client Player3 { get; set; }
+        public Client Player4 { get; set; }
+        public Client Player5 { get; set; }
+        public string WinnerName { get; set; }
+
+        public void Play()
+        {
+            //TODO
+            //params:
+            //eg. card position, player, board size...
+        }
+
+        public void CheckWinner(List<Client> players)
+        {
+            int maxPoint = 0;
+            foreach (var player in players)
+            {
+                if (player.Points > maxPoint)
+                {
+                    maxPoint = player.Points;
+                    WinnerName = player.Name;
+                }
+            }
+        }
     }
 }
