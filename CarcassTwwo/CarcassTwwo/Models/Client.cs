2f5dc1f77e8108e6cb5bed281a0262305eec49a2
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-22 17:38:47 +0200

    Add Client to Models

---
+++ CarcassTwwo/CarcassTwwo/Models/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarcassTwwo.Models
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        [NotMapped]
+        public List<Client> CoPlayers { get; set; }
+        public bool IsPlaying { get; set; }
+        public bool IsWaitingForMove { get; set; }
+        public string ConnectionId { get; set; }
+    }
+}
