933c9e8678748c8480b064dccc002d8de5c11cf3
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-22 16:10:42 +0200

    Create IConnectionManager interface

---
+++ CarcassTwwo/CarcassTwwo/Hubs/IConnectionManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CarcassTwwo.Hubs
+{
+    public interface IConnectionManager
+    {
+        void AddConnection(string username, string connectionId);
+        void RemoveConnection(string connectionId);
+        HashSet<string> GetConnections(string username);
+        IEnumerable<string> OnlineUsers { get; }
+    }
+}
