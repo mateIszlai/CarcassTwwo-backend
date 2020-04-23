f901cb085d7608bde8c0e78ba722e3a875f67d79
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-23 14:55:05 +0200

    Create LocalGroup class

---
+++ CarcassTwwo/CarcassTwwo/Models/LocalGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarcassTwwo.Models
+{
+    public class LocalGroup
+    {
+        public string Name { get; set; }
+        public string OwnerName { get; set; }
+        public List<Client> Members { get; set; }
+    }
+}
