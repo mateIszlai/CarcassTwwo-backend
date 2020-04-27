62609220f0a7c3787522c22d2ad5b1a20fda4b5f
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-27 21:06:55 +0200

    Create model for tile

---
+++ CarcassTwwo/CarcassTwwo/Models/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarcassTwwo.Models
+{
+    public class Tile
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public LandType Field1 { get; set; }
+        public LandType Field2 { get; set; }
+        public LandType Field3 { get; set; }
+        public LandType Field4 { get; set; }
+        public LandType Field5 { get; set; }
+        public LandType Field6 { get; set; }
+        public LandType Field7 { get; set; }
+        public LandType Field8 { get; set; }
+        public LandType Field9 { get; set; }
+
+        public int Amount { get; set; }
+        public int Remaining { get; set; }
+    }
+}
