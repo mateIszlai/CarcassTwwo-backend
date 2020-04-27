165c940d87c1b236e283c1937221b9c3ff454e88
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-27 21:10:18 +0200

    Create model for card

---
+++ CarcassTwwo/CarcassTwwo/Models/Card.cs
@@ -0,0 +1,38 @@
+﻿namespace CarcassTwwo.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public int GameId { get; set; }
+        public Carcassonne Game { get; set; }
+        public Tile Tile { get; set; }
+        public int MeepleField { get; set; }
+        public Coordinate Coordinate { get; set; }
+
+        public LandType Top { get; set; }
+        public LandType Bottom { get; set; }
+        public LandType Left { get; set; }
+        public LandType Right { get; set; }
+
+        public bool TopIsFree { get; set; }
+        public bool BottomIsFree { get; set; }
+        public bool LeftIsFree { get; set; }
+        public bool RightIsFree { get; set; }
+
+        public bool HasCrest { get; set; }
+
+        public Card(Tile tile)
+        {
+            Tile = tile;
+            Top = Tile.Field2;
+            Bottom = Tile.Field8;
+            Left = Tile.Field4;
+            Right = Tile.Field6;
+        }
+
+        public void SetSideOccupation()
+        {
+            //TODO: this will change the occupation of a side from free to taken
+        }
+    }
+}
