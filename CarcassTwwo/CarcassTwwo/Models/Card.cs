784b75182c35e48495be0c1a94ca9b4e82f176b4
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-28 15:23:45 +0200

    Change constructor and add new methods to Card

--- CarcassTwwo/CarcassTwwo/Models/Card.cs
+++ CarcassTwwo/CarcassTwwo/Models/Card.cs
@@ -20,21 +20,38 @@
         public bool BottomIsFree { get; set; }
         public bool LeftIsFree { get; set; }
         public bool RightIsFree { get; set; }
-
         public bool HasCrest { get; set; }
 
         public Card(Tile tile)
         {
             Tile = tile;
-            Top = Tile.Field2;
-            Bottom = Tile.Field8;
-            Left = Tile.Field4;
-            Right = Tile.Field6;
+            Top = Tile.Field2.LandType;
+            Bottom = Tile.Field8.LandType;
+            Left = Tile.Field4.LandType;
+            Right = Tile.Field6.LandType;
+            HasCrest = Tile.HasCrest;
         }
 
-        public void SetSideOccupation()
+        public void SetSideOccupation(bool top, bool bottom, bool left, bool right)
         {
+            TopIsFree = top;
+            BottomIsFree = bottom;
+            LeftIsFree = left;
+            RightIsFree = right;
+
             //TODO: this will change the occupation of a side from free to taken
+        }
+
+        public void PlaceMeeple(Field field)
+        {
+            MeepleField = field.Coordinate;
+            MeepleType = field.LandType.Meeple;
+            HasMeeple = true;
+        }
+
+        public void RemoveMeeple()
+        {
+            HasMeeple = false;
         }
     }
 }
