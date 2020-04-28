b91438572bd2de48c664982ae63c3cf9c5e5af23
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-28 15:22:30 +0200

    Use field instead of landtype in Tile

--- CarcassTwwo/CarcassTwwo/Models/Tile.cs
+++ CarcassTwwo/CarcassTwwo/Models/Tile.cs
@@ -9,17 +9,31 @@ namespace CarcassTwwo.Models
     {
         public int Id { get; set; }
         public string Image { get; set; }
-        public LandType Field1 { get; set; }
-        public LandType Field2 { get; set; }
-        public LandType Field3 { get; set; }
-        public LandType Field4 { get; set; }
-        public LandType Field5 { get; set; }
-        public LandType Field6 { get; set; }
-        public LandType Field7 { get; set; }
-        public LandType Field8 { get; set; }
-        public LandType Field9 { get; set; }
+        public Field Field1 { get; set; }
+        public Field Field2 { get; set; }
+        public Field Field3 { get; set; }
+        public Field Field4 { get; set; }
+        public Field Field5 { get; set; }
+        public Field Field6 { get; set; }
+        public Field Field7 { get; set; }
+        public Field Field8 { get; set; }
+        public Field Field9 { get; set; }
+
+        public bool HasCrest { get; set; }
 
         public int Amount { get; set; }
         public int Remaining { get; set; }
+        public Tile()
+        {
+            Field1.Coordinate = new Coordinate { x = 0, y = 0 };
+            Field2.Coordinate = new Coordinate { x = 1, y = 0 };
+            Field3.Coordinate = new Coordinate { x = 2, y = 0 };
+            Field4.Coordinate = new Coordinate { x = 0, y = 1 };
+            Field5.Coordinate = new Coordinate { x = 1, y = 1 };
+            Field6.Coordinate = new Coordinate { x = 2, y = 1 };
+            Field7.Coordinate = new Coordinate { x = 0, y = 2 };
+            Field8.Coordinate = new Coordinate { x = 1, y = 2 };
+            Field9.Coordinate = new Coordinate { x = 2, y = 2 };
+        }
     }
 }
