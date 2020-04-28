927ba4f5c3ae4066b7803f843b133b796217a3a6
dolezsariza <izabella.dolezsar@gmail.com>
2020-04-28 15:20:22 +0200

    Add available coordinates to Board

--- CarcassTwwo/CarcassTwwo/Models/Board.cs
+++ CarcassTwwo/CarcassTwwo/Models/Board.cs
@@ -8,7 +8,18 @@ namespace CarcassTwwo.Models
     public class Board
     {
         public Dictionary<Coordinate, Card> CardCoordinates { get; set; }
+        public List<Coordinate> AvailableCoordinates { get; set; }
+        public void AddAvailableCoordinate(Coordinate coordinate)
+        {
+            AvailableCoordinates.Add(coordinate);
+        }
+
+        public void RemoveFromCoordinates(Coordinate coordinate)
+        {
+            AvailableCoordinates.Remove(coordinate);
+        }
     }
+
 
     public struct Coordinate
     {
