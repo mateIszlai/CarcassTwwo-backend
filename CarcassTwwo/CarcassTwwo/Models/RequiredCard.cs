d05ce761b60d09fdc9fc68c6ae0c1186ef360fd4
dolezsariza <izabella.dolezsar@gmail.com>
2020-05-04 19:24:05 +0200

    Move UpdateRequiredCard method to RequiredCard class

--- CarcassTwwo/CarcassTwwo/Models/RequiredCard.cs
+++ CarcassTwwo/CarcassTwwo/Models/RequiredCard.cs
@@ -11,6 +11,7 @@ namespace CarcassTwwo.Models
         public LandType Bottom { get; set; }
         public LandType Left { get; set; }
         public LandType Right { get; set; }
+        public Coordinate Coordinate { get; set; }
 
         public RequiredCard(LandType top, LandType bottom, LandType left, LandType right)
         {
@@ -18,6 +19,31 @@ namespace CarcassTwwo.Models
             Bottom = bottom;
             Left = left;
             Right = right;
+        }
+
+        public void UpdateRequiredCard(Dictionary<Coordinate, Card> CardCoordinates)
+        {
+            var left = new Coordinate { x = Coordinate.x - 1, y = Coordinate.y };
+            var right = new Coordinate { x = Coordinate.x + 1, y = Coordinate.y };
+            var top = new Coordinate { x = Coordinate.x, y = Coordinate.y + 1 };
+            var bottom = new Coordinate { x = Coordinate.x, y = Coordinate.y - 1 };
+
+            if (CardCoordinates.ContainsKey(left))
+            {
+                Left = CardCoordinates[left].Right;
+            }
+            if (CardCoordinates.ContainsKey(right))
+            {
+                Right = CardCoordinates[right].Left;
+            }
+            if (CardCoordinates.ContainsKey(top))
+            {
+                Top = CardCoordinates[top].Bottom;
+            }
+            if (CardCoordinates.ContainsKey(bottom))
+            {
+                Bottom = CardCoordinates[bottom].Top;
+            }
         }
     }
 }
