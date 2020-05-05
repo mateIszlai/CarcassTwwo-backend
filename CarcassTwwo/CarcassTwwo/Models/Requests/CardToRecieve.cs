ba9688317cccd90b7138d78b978f7c9205b76828
Mate Iszlai <iszimate@gmail.com>
2020-05-05 18:38:33 +0200

    Add CardToRecieve request model class

---
+++ CarcassTwwo/CarcassTwwo/Models/Requests/CardToRecieve.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarcassTwwo.Models.Requests
+{
+    public class CardToRecieve
+    {
+        public int CardId { get; set; }
+        public int Rotation { get; set; }
+        public Coordinate Coordinate { get; set; }
+    }
+}
