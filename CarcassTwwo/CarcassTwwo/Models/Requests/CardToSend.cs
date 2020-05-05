a7ec46da8d902eb13204af4c1c2efa484e8772a2
Mate Iszlai <iszimate@gmail.com>
2020-05-05 18:38:04 +0200

    Add CardToSend request model class

---
+++ CarcassTwwo/CarcassTwwo/Models/Requests/CardToSend.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarcassTwwo.Models.Requests
+{
+    public class CardToSend
+    {
+        public CardToSend (int tileId)
+        {
+            TileId = tileId;
+            CoordinatesWithRotations = new List<CoordinatesWithRotation>();
+        }
+        public int TileId { get; private set; }
+        public List<CoordinatesWithRotation> CoordinatesWithRotations { get; set; }
+    }
+}
