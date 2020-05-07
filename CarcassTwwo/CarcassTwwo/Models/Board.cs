b6b46903c227e23fb3884371185c0573b555cd87
dolezsariza <izabella.dolezsar@gmail.com>
2020-05-07 18:21:12 +0200

    Remove setting of adjacent cards' side's availability

--- CarcassTwwo/CarcassTwwo/Models/Board.cs
+++ CarcassTwwo/CarcassTwwo/Models/Board.cs
@@ -93,25 +93,17 @@ namespace CarcassTwwo.Models
             }
 
             var vertical = new Coordinate { x = card.Coordinate.x, y = card.Coordinate.y + 1 };
-
             card.TopIsFree = CardCoordinates.ContainsKey(vertical) ? false : true;
-            if (!card.TopIsFree)
-                CardCoordinates[vertical].BottomIsFree = false;
-
+            
             vertical.y = card.Coordinate.y - 1;
             card.BottomIsFree = CardCoordinates.ContainsKey(vertical) ? false : true;
-            if (!card.BottomIsFree)
-                CardCoordinates[vertical].TopIsFree = false;
 
             var horizontal = new Coordinate { x = card.Coordinate.x + 1, y = card.Coordinate.y };
             card.RightIsFree = CardCoordinates.ContainsKey(horizontal) ? false : true;
-            if (!card.RightIsFree)
-                CardCoordinates[horizontal].LeftIsFree = false;
-
+           
             horizontal.x = card.Coordinate.x - 1;
             card.LeftIsFree = CardCoordinates.ContainsKey(horizontal) ? false : true;
-            if (!card.LeftIsFree)
-                CardCoordinates[horizontal].RightIsFree = false;
+           
         }
     }
 }
