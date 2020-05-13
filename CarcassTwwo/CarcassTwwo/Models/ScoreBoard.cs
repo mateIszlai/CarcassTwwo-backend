02eaefcdac6fc8d58c88a8d7da1a3ef94857bcbc
dolezsariza <izabella.dolezsar@gmail.com>
2020-05-13 19:57:49 +0200

    Add base of CountPointsAfterRound to ScoreBoard

--- CarcassTwwo/CarcassTwwo/Models/ScoreBoard.cs
+++ CarcassTwwo/CarcassTwwo/Models/ScoreBoard.cs
@@ -16,5 +16,14 @@ namespace CarcassTwwo.Models
 
         //This class could be used to keep track of every players' score
         //like in the real game
+
+        public void CountPointsAfterRound()
+        {
+            //this should get data from the game/board, and use it to update the points e.g.:
+            foreach(Client player in players)
+            {
+                player.Points += 42;
+            }
+        }
     }
 }
