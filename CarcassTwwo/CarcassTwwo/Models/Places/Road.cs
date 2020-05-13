23a2a0cfda2061221fa89f834fcfd946cef9f4f4
dolezsariza <izabella.dolezsar@gmail.com>
2020-05-13 19:56:22 +0200

    Add base for Road

--- CarcassTwwo/CarcassTwwo/Models/Places/Road.cs
+++ CarcassTwwo/CarcassTwwo/Models/Places/Road.cs
@@ -7,5 +7,28 @@ namespace CarcassTwwo.Models.Places
 {
     public class Road
     {
+        public List<Meeple> HighwayMen { get; set; }
+        public List<Coordinate> RoadTiles { get; set; }
+        public bool IsFinished { get; set; }
+        public Road()
+        {
+            HighwayMen = new List<Meeple>();
+            RoadTiles = new List<Coordinate>();
+        }
+
+        public void ExpandRoad(Coordinate coordinate)
+        {
+            RoadTiles.Add(coordinate);
+        }
+
+        public void PlaceHighwayMan(Coordinate field)
+        {
+            HighwayMen.Add(new Meeple(field, "Highwayman"));
+        }
+
+        public void CheckStateOfRoad()
+        {
+            //TODO if road is finished then IsFinished = true;
+        }
     }
 }
