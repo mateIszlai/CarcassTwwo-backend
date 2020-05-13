2cdd8e47ae5e2b9735e526ff51d102e1850d7843
dolezsariza <izabella.dolezsar@gmail.com>
2020-05-13 19:56:30 +0200

    Add base for Monastery

--- CarcassTwwo/CarcassTwwo/Models/Places/Monastery.cs
+++ CarcassTwwo/CarcassTwwo/Models/Places/Monastery.cs
@@ -7,5 +7,27 @@ namespace CarcassTwwo.Models.Places
 {
     public class Monastery
     {
+        public Meeple Monk { get; set; }
+        public List<Coordinate> SurroundingTiles { get; set; }
+        public bool IsFinished { get; set; }
+        public Monastery()
+        {
+            SurroundingTiles = new List<Coordinate>();
+        }
+
+        public void PlaceMonk(Coordinate field)
+        {
+            Monk = new Meeple(field, "Monk");
+        }
+
+        public void ExpandMonastery(Coordinate coordinate)
+        {
+            SurroundingTiles.Add(coordinate);
+        }
+
+        public void CheckStateOfMonastery()
+        {
+            //TODO if city is finished then IsFinished = true;
+        }
     }
 }
