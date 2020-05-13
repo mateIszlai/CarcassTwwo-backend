208be9ce6a41843c7a696be045a8202c52941c66
dolezsariza <izabella.dolezsar@gmail.com>
2020-05-13 19:56:39 +0200

    Add base for GrassLand

--- CarcassTwwo/CarcassTwwo/Models/Places/GrassLand.cs
+++ CarcassTwwo/CarcassTwwo/Models/Places/GrassLand.cs
@@ -7,5 +7,20 @@ namespace CarcassTwwo.Models.Places
 {
     public class GrassLand
     {
+        //Grasslands are counted only at the end of the game.
+        public int SurroundingCities { get; set; }
+        //only the finished cities count
+        public List<Meeple> Peasants { get; set; }
+        public GrassLand()
+        {
+            SurroundingCities = 0;
+            Peasants = new List<Meeple>();
+        }
+
+        public void AddNewCity()
+        {
+            SurroundingCities++;
+        }
+
     }
 }
