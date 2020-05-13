ca71bf9dac653a59d4d965f4c2ac1aa1c5ae367b
dolezsariza <izabella.dolezsar@gmail.com>
2020-05-13 19:56:54 +0200

    Add base for City

--- CarcassTwwo/CarcassTwwo/Models/Places/City.cs
+++ CarcassTwwo/CarcassTwwo/Models/Places/City.cs
@@ -7,5 +7,28 @@ namespace CarcassTwwo.Models.Places
 {
     public class City
     {
+        public List<Meeple> Knights { get; set; }
+        public List<Coordinate> CityTiles { get; set; }
+        public bool IsFinished { get; set; }
+        public City()
+        {
+            Knights = new List<Meeple>();
+            CityTiles = new List<Coordinate>();
+        }
+
+        public void BuildCity(Coordinate coordinate)
+        {
+            CityTiles.Add(coordinate);
+        }
+
+        public void PlaceKnight(Coordinate field)
+        {
+            Knights.Add(new Meeple(field, "Knight"));
+        }
+
+        public void CheckStateOfCity()
+        {
+            //TODO if city is finished then IsFinished = true;
+        }
     }
 }
