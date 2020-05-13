b1f8a5ef46cc97127b2b93b4ca95eed0e5ca57ee
dolezsariza <izabella.dolezsar@gmail.com>
2020-05-13 19:31:19 +0200

    Create base of ScoreBoard

---
+++ CarcassTwwo/CarcassTwwo/Models/ScoreBoard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarcassTwwo.Models
+{
+    public class ScoreBoard
+    {
+        HashSet<Client> players;
+
+        public ScoreBoard(HashSet<Client> players)
+        {
+            this.players = players;
+        }
+
+        //This class could be used to keep track of every players' score
+        //like in the real game
+    }
+}
