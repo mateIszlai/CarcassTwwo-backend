e6f2261da350d19b2e673e50a166ca8d053a2b08
Mate Iszlai <iszimate@gmail.com>
2020-05-20 16:25:50 +0200

    Modify GrassLand to exteds Place

--- CarcassTwwo/CarcassTwwo/Models/Places/GrassLand.cs
+++ CarcassTwwo/CarcassTwwo/Models/Places/GrassLand.cs
@@ -5,16 +5,26 @@ using System.Threading.Tasks;
 
 namespace CarcassTwwo.Models.Places
 {
-    public class GrassLand
+    public class GrassLand : Place
     {
+
         //Grasslands are counted only at the end of the game.
         public int SurroundingCities { get; set; }
+        public int Size { get { return _cardIds.Count; }}
+
+        private List<int> _cardIds;
         //only the finished cities count
         public List<Meeple> Peasants { get; set; }
-        public GrassLand()
+        public GrassLand(int id) : base(id)
         {
             SurroundingCities = 0;
             Peasants = new List<Meeple>();
+            _cardIds = new List<int>();
+        }
+
+        public void ExpandLand (int cardId)
+        {
+            _cardIds.Add(cardId);
         }
 
         public void AddNewCity()
@@ -22,5 +32,9 @@ namespace CarcassTwwo.Models.Places
             SurroundingCities++;
         }
 
+        public override void PlaceMeeple(Client owner)
+        {
+            Meeples.Add(new Meeple(MeepleType.PEASANT, owner));
+        }
     }
 }
