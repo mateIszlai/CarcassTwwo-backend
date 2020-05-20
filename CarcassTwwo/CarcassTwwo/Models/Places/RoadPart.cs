6c047c6edac53992d137f791b7aea5bb4eb6397c
Mate Iszlai <iszimate@gmail.com>
2020-05-20 16:14:50 +0200

    Create RoadPart

---
+++ CarcassTwwo/CarcassTwwo/Models/Places/RoadPart.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CarcassTwwo.Models.Places
+{
+    public class RoadPart
+    {
+        public List<int> CardIds { get; set; }
+        public bool LeftOpen { get; set; }
+        public bool RightOpen { get; set; }
+    }
+}
