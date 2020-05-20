d82021ab3f6d7bea42bef30f2905f009794c5a3d
Mate Iszlai <iszimate@gmail.com>
2020-05-20 16:12:37 +0200

    Create CityPart

---
+++ CarcassTwwo/CarcassTwwo/Models/Places/CityPart.cs
@@ -0,0 +1,16 @@
+﻿namespace CarcassTwwo.Models.Places
+{
+    public class CityPart
+    {
+        public int CardId { get; set; }
+        public bool TopIsOpen { get; set; }
+        public bool LeftIsOpen { get; set; }
+        public bool BottomIsOpen { get; set; }
+        public bool RightIsOpen { get; set; }
+
+        public CityPart(int cardId)
+        {
+            CardId = cardId;
+        }
+    }
+}
