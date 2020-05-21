a576384da2337f5b7b1ef042d336a30e500c9ecc
Mate Iszlai <iszimate@gmail.com>
2020-05-21 11:38:01 +0200

    Add Lands property

--- CarcassTwwo/CarcassTwwo/Models/Places/CityPart.cs
+++ CarcassTwwo/CarcassTwwo/Models/Places/CityPart.cs
@@ -1,4 +1,6 @@
-﻿namespace CarcassTwwo.Models.Places
+﻿using System.Collections.Generic;
+
+namespace CarcassTwwo.Models.Places
 {
     public class CityPart
     {
@@ -7,10 +9,13 @@
         public bool LeftIsOpen { get; set; }
         public bool BottomIsOpen { get; set; }
         public bool RightIsOpen { get; set; }
+        public List<int> Lands { get; set; }
 
         public CityPart(int cardId)
         {
             CardId = cardId;
+            LeftIsOpen = BottomIsOpen = RightIsOpen = TopIsOpen = true;
+            Lands = new List<int>();
         }
     }
 }
--- CarcassTwwo/CarcassTwwo/Models/Places/RoadPart.cs
+++ CarcassTwwo/CarcassTwwo/Models/Places/RoadPart.cs
@@ -4,8 +4,16 @@ namespace CarcassTwwo.Models.Places
 {
     public class RoadPart
     {
-        public List<int> CardIds { get; set; }
+        public int CardId { get; private set; }
         public bool LeftOpen { get; set; }
         public bool RightOpen { get; set; }
+        public List<int> Lands { get; set; }
+
+        public RoadPart(int cardId)
+        {
+            CardId = cardId;
+            LeftOpen = RightOpen = true;
+            Lands = new List<int>();
+        }
     }
 }
