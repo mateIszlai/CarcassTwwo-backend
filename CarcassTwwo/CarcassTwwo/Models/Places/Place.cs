1f4620e2ce4de065f8f26304ea1c2980c90fc6a8
dolezsariza <izabella.dolezsar@gmail.com>
2020-06-13 14:58:32 +0200

    Add IsCounted to Place to know if place was already checked in scoreboard

--- CarcassTwwo/CarcassTwwo/Models/Places/Place.cs
+++ CarcassTwwo/CarcassTwwo/Models/Places/Place.cs
@@ -7,11 +7,13 @@ namespace CarcassTwwo.Models.Places
         public int Id { get; private set; }
         public List<Meeple> Meeples { get; private set; }
         public bool CanPlaceMeeple { get; set; }
+        public bool IsCounted { get; set; }
 
         public Place(int id)
         {
             Id = id;
             Meeples = new List<Meeple>();
+            IsCounted = false;
         }
         public abstract void PlaceMeeple(Client owner, int field, Card card);
         public void RemoveMeeples()
