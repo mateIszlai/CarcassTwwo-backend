44bff2072a5d44e944fa8b7fd8c2b66570b8bc50
dolezsariza <izabella.dolezsar@gmail.com>
2020-06-13 15:01:22 +0200

    Add CheckOwnerOfCity method

--- CarcassTwwo/CarcassTwwo/Models/ScoreBoard.cs
+++ CarcassTwwo/CarcassTwwo/Models/ScoreBoard.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarcassTwwo.Models.Places;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
@@ -15,6 +16,17 @@ namespace CarcassTwwo.Models
             players.ToList().ForEach(pl => Players[pl] = 0);
         }
 
+        internal void CheckOwnerOfCity(City city)
+        {
+            Dictionary<Client, int> meepleCountByPlayers = new Dictionary<Client, int>();
+            foreach(var meeple in city.Meeples)
+            {
+                _ = meepleCountByPlayers[meeple.Owner] != 0 ? meepleCountByPlayers[meeple.Owner]++ : meepleCountByPlayers[meeple.Owner] = 1;
+            }
+            int maxCount = meepleCountByPlayers.Values.Max();
+            var cityOwners = meepleCountByPlayers.Keys.Where(k => meepleCountByPlayers[k] == maxCount).ToList();
+            AddPointsForCity(cityOwners, city);
+        }
 
         {
             //this should get data from the game/board, and use it to update the points e.g.:
