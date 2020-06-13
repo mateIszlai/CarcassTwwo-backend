626200afcbe543839dd8c02cfc1096aba3966367
dolezsariza <izabella.dolezsar@gmail.com>
2020-06-13 15:00:45 +0200

    Add base of ScoreBoard

--- CarcassTwwo/CarcassTwwo/Models/ScoreBoard.cs
+++ CarcassTwwo/CarcassTwwo/Models/ScoreBoard.cs
@@ -1,23 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 
 namespace CarcassTwwo.Models
 {
     public class ScoreBoard
     {
-        HashSet<Client> players;
+        public Dictionary<Client, int> Players { get; private set; }
 
         public ScoreBoard(HashSet<Client> players)
         {
-            this.players = players;
+            players.ToList().ForEach(pl => Players[pl] = 0);
         }
 
-        //This class could be used to keep track of every players' score
-        //like in the real game
 
-        public void CountPointsAfterRound()
         {
             //this should get data from the game/board, and use it to update the points e.g.:
             foreach(Client player in players)
