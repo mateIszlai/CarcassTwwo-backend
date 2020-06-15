c77de320c7833e821b647789b5b42886afc1c24f
dolezsariza <izabella.dolezsar@gmail.com>
2020-06-15 14:36:12 +0200

    Create MeepleInfo

---
+++ CarcassTwwo/CarcassTwwo/Models/Requests/MeepleInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarcassTwwo.Models.Requests
+{
+    public class MeepleInfo
+    {
+        Client Owner { get; set; }
+        Coordinate Coordinate { get; set; }
+        int Field { get; set; }
+        public MeepleInfo(Client owner, Coordinate coordinate, int field)
+        {
+            Owner = owner;
+            Coordinate = coordinate;
+            Field = field;
+        }
+    }
+}
