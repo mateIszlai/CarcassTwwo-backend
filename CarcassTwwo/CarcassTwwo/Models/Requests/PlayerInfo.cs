72381d074dd7dc89077d1366dafb95d9c8d62488
Mate Iszlai <iszimate@gmail.com>
2020-06-15 13:42:56 +0200

    Create PlayerInfo class

---
+++ CarcassTwwo/CarcassTwwo/Models/Requests/PlayerInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace CarcassTwwo.Models.Requests
+{
+    public class PlayerInfo
+    {
+        public int MeepleCount { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
