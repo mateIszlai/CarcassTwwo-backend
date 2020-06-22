96b6e9f1b8a6100f81ba7d2e29f54e8a7cdd009b
Mate Iszlai <iszimate@gmail.com>
2020-06-22 16:33:00 +0200

    Create ILandHandler interface

---
+++ CarcassTwwo/CarcassTwwo/Models/Requests/ILandHandler.cs
@@ -0,0 +1,10 @@
+﻿using CarcassTwwo.Models.Places;
+using System.Collections.Generic;
+
+namespace CarcassTwwo.Models.Requests
+{
+    public interface ILandHandler
+    {
+        public HashSet<GrassLand> Lands { get; }
+    }
+}
