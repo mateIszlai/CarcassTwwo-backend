ade6c29763c4415d56497b87379c69c71151c424
Mate Iszlai <iszimate@gmail.com>
2020-06-22 16:50:24 +0200

    Add PlacTwoCity method

--- CarcassTwwo/CarcassTwwo/Models/Requests/CityHandler.cs
+++ CarcassTwwo/CarcassTwwo/Models/Requests/CityHandler.cs
@@ -18,6 +18,107 @@ namespace CarcassTwwo.Models.Requests
             _landHandler = landHandler;
         }
 
+        private void PlaceTwoCity(Card topCard, Card botCard, Card leftCard, Card rightCard, Card card, int id)
+        {
+            if (card.Top.Name == "City")
+            {
+                if (topCard != null)
+                {
+                    var city = Cities.FirstOrDefault(c => c.Id == topCard.Bottom.PlaceId);
+                    card.SetField(Side.TOP, city.Id);
+                    if (city.GetCityPartByCardId(card.Id) == null)
+                        city.ExpandCity(new CityPart(card.Id) { TopIsOpen = false, LeftIsOpen = false, BottomIsOpen = false, RightIsOpen = false, HasCrest = card.HasCrest });
+                    city.SetSides(topCard.Id, Side.BOTTOM);
+                    _landHandler.Lands.First(l => l.Id == card.Tile.Field5.PlaceId).SurroundingCities.Add(city.Id);
+                }
+                else
+                {
+                    id++;
+                    var city = new City(id);
+                    city.ExpandCity(new CityPart(card.Id) { TopIsOpen = true, LeftIsOpen = false, BottomIsOpen = false, RightIsOpen = false, HasCrest = card.HasCrest });
+                    Cities.Add(city);
+                    card.SetField(Side.TOP, id);
+                    _landHandler.Lands.First(l => l.Id == card.Tile.Field5.PlaceId).SurroundingCities.Add(city.Id);
+                }
+
+            }
+
+            if (card.Left.Name == "City")
+            {
+
+                if (leftCard != null)
+                {
+                    var city = Cities.FirstOrDefault(c => c.Id == leftCard.Right.PlaceId);
+                    card.SetField(Side.MIDDLELEFT, city.Id);
+                    if (city.GetCityPartByCardId(card.Id) == null)
+                        city.ExpandCity(new CityPart(card.Id) { TopIsOpen = false, LeftIsOpen = false, BottomIsOpen = false, RightIsOpen = false, HasCrest = card.HasCrest });
+                    city.SetSides(leftCard.Id, Side.MIDDLERIGHT);
+                    _landHandler.Lands.First(l => l.Id == card.Tile.Field5.PlaceId).SurroundingCities.Add(city.Id);
+
+                }
+                else
+                {
+                    id++;
+                    var city = new City(id);
+                    city.ExpandCity(new CityPart(card.Id) { TopIsOpen = false, LeftIsOpen = true, BottomIsOpen = false, RightIsOpen = false, HasCrest = card.HasCrest });
+                    Cities.Add(city);
+                    card.SetField(Side.MIDDLELEFT, id);
+                    _landHandler.Lands.First(l => l.Id == card.Tile.Field5.PlaceId).SurroundingCities.Add(city.Id);
+                }
+            }
+
+            if (card.Bottom.Name == "City")
+            {
+                if (botCard != null)
+                {
+                    var city = Cities.FirstOrDefault(c => c.Id == botCard.Top.PlaceId);
+                    card.SetField(Side.BOTTOM, city.Id);
+                    if (city.GetCityPartByCardId(card.Id) == null)
+                        city.ExpandCity(new CityPart(card.Id) { TopIsOpen = false, LeftIsOpen = false, BottomIsOpen = false, RightIsOpen = false, HasCrest = card.HasCrest });
+                    city.SetSides(botCard.Id, Side.TOP);
+                    _landHandler.Lands.First(l => l.Id == card.Tile.Field5.PlaceId).SurroundingCities.Add(city.Id);
+
+                }
+                else
+                {
+                    id++;
+                    var city = new City(id);
+                    city.ExpandCity(new CityPart(card.Id) { TopIsOpen = false, LeftIsOpen = false, BottomIsOpen = true, RightIsOpen = false, HasCrest = card.HasCrest });
+                    Cities.Add(city);
+                    card.SetField(Side.BOTTOM, id);
+                    _landHandler.Lands.First(l => l.Id == card.Tile.Field5.PlaceId).SurroundingCities.Add(city.Id);
+
+                }
+            }
+
+            if (card.Right.Name == "City")
+            {
+
+                if (rightCard != null)
+                {
+                    var city = Cities.FirstOrDefault(c => c.Id == rightCard.Left.PlaceId);
+                    card.SetField(Side.MIDDLERIGHT, city.Id);
+                    if (city.GetCityPartByCardId(card.Id) == null)
+                        city.ExpandCity(new CityPart(card.Id) { TopIsOpen = false, LeftIsOpen = false, BottomIsOpen = false, RightIsOpen = false, HasCrest = card.HasCrest });
+                    city.SetSides(rightCard.Id, Side.MIDDLELEFT);
+                    _landHandler.Lands.First(l => l.Id == card.Tile.Field5.PlaceId).SurroundingCities.Add(city.Id);
+
+                }
+                else
+                {
+                    id++;
+                    var city = new City(id);
+                    city.ExpandCity(new CityPart(card.Id) { TopIsOpen = false, LeftIsOpen = false, BottomIsOpen = false, RightIsOpen = true, HasCrest = card.HasCrest });
+                    Cities.Add(city);
+                    card.SetField(Side.MIDDLERIGHT, id);
+                    _landHandler.Lands.First(l => l.Id == card.Tile.Field5.PlaceId).SurroundingCities.Add(city.Id);
+                }
+            }
+
+            return;
+        }
+
+
         private void AddCityToLand(Card card, int landCounts, int cityId)
         {
             if (landCounts == 0)
