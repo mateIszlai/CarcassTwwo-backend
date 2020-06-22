e1b6464996a861f90dbd357bb8ad4329fe15073a
Mate Iszlai <iszimate@gmail.com>
2020-06-22 16:35:54 +0200

    Create AbstractHandler base handler class that implements IHandler interface

---
+++ CarcassTwwo/CarcassTwwo/Models/Requests/AbstractHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace CarcassTwwo.Models.Requests
+{
+    public abstract class AbstractHandler : IHandler
+    {
+        private IHandler _nextHandler;
+
+        public virtual int Handle(Card topCard, Card botCard, Card leftCard, Card rightCard, Card card, int landCounts, int id, bool roadClosed)
+        {
+            if(_nextHandler != null)
+            {
+                _nextHandler.Handle(topCard, botCard, leftCard, rightCard, card, landCounts, id, roadClosed);
+            }
+            return id;
+        }
+
+        public IHandler SetNext(IHandler handler)
+        {
+            _nextHandler = handler;
+            return handler;
+        }
+    }
+}
