fd193f85f14f7e789b4deac310808b6ecaa533d0
Mate Iszlai <iszimate@gmail.com>
2020-06-22 17:14:27 +0200

    Create IBoard interface

---
+++ CarcassTwwo/CarcassTwwo/Models/IBoard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarcassTwwo.Models
+{
+    public interface IBoard
+    {
+         Dictionary<Coordinate, Card> CardCoordinates { get; set; }
+    }
+}
