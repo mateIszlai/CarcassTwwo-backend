c1c0e5e76655c956b446a5063f17f03ddd073a7f
Mate Iszlai <iszimate@gmail.com>
2020-06-23 13:47:07 +0200

    Add Roads property

--- CarcassTwwo/CarcassTwwo/Models/Requests/RoadHandler.cs
+++ CarcassTwwo/CarcassTwwo/Models/Requests/RoadHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarcassTwwo.Models.Places;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,6 @@ namespace CarcassTwwo.Models.Requests
 {
     public class RoadHandler : AbstractHandler
     {
-
+        public HashSet<Road> Roads { get; set; }
     }
 }
