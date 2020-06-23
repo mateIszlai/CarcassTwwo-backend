8ce108391dd94feaf221eca8949d1ab70013f34c
Mate Iszlai <iszimate@gmail.com>
2020-06-23 14:05:58 +0200

    Create LandHandler class that extends AbstractHandler and implements ILandHandler

---
+++ CarcassTwwo/CarcassTwwo/Models/Requests/LandHandler.cs
@@ -0,0 +1,13 @@
+﻿using CarcassTwwo.Models.Places;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarcassTwwo.Models.Requests
+{
+    public class LandHandler : AbstractHandler, ILandHandler
+    {
+        public HashSet<GrassLand> Lands { get; set; }
+    }
+}
