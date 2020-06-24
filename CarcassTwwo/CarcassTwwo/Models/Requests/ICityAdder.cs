31d4306f58b4e958014f9194215af1cb604d8eec
Mate Iszlai <iszimate@gmail.com>
2020-06-24 17:50:23 +0200

    Create ICityAdder interface

---
+++ CarcassTwwo/CarcassTwwo/Models/Requests/ICityAdder.cs
@@ -0,0 +1,8 @@
+﻿namespace CarcassTwwo.Models.Requests
+{
+    public interface ICityAdder
+    {
+        void AddCityToLand(Card card, int landCounts, int cityId);
+        void ChangeCityIdInLand(int landId, int cityIdToChange, int newCityId);
+    }
+}
