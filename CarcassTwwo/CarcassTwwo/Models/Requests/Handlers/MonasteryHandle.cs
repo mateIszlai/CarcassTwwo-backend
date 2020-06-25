a36188d117858d44762f360fec2a0800d4e71f71
Mate Iszlai <iszimate@gmail.com>
2020-06-25 12:57:12 +0200

    Create constructor, override Handle method

--- CarcassTwwo/CarcassTwwo/Models/Requests/Handlers/MonasteryHandle.cs
+++ CarcassTwwo/CarcassTwwo/Models/Requests/Handlers/MonasteryHandle.cs
@@ -14,6 +14,29 @@ namespace CarcassTwwo.Models.Requests.Handlers
             private set { _monasteries = value; }
         }
 
+        public MonasteryHandle(IBoard board) : base(board)
+        {
+            Monasteries = new HashSet<Monastery>();
+        }
+
+        public override int Handle(Card topCard, Card botCard, Card leftCard, Card rightCard, Card card, int landCounts, int id, bool roadClosed, Coordinate[] surroundingCoords)
+        {
+            SetNearMonasteries(card.Coordinate);
+            if (surroundingCoords == null)
+                return base.Handle(topCard, botCard, leftCard, rightCard, card, landCounts, id, roadClosed, surroundingCoords);
+            
+            id++;
+            var monastery = new Monastery(id, card.Coordinate);
+            card.MonasteryId = id;
+            surroundingCoords.ToList().ForEach(coord => {
+                if (_board.CardCoordinates.ContainsKey(coord))
+                    monastery.SurroundingCoordinates.Remove(coord);
+            });
+            _monasteries.Add(monastery);
+            card.SetField(Side.MIDDLE, id);
+            return base.Handle(topCard, botCard, leftCard, rightCard, card, landCounts, id, roadClosed, surroundingCoords);
+        }
+
         private void SetNearMonasteries(Coordinate coordinate)
         {
             var monasteries = _monasteries.Where(m => m.SurroundingCoordinates.Contains(coordinate)).ToList();
