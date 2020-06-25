8b0e99436ec22e149c217ecdea9bcb4a1ddfe98a
Mate Iszlai <iszimate@gmail.com>
2020-06-25 11:58:09 +0200

    Create MonasteryHandle class

---
+++ CarcassTwwo/CarcassTwwo/Models/Requests/Handlers/MonasteryHandle.cs
@@ -0,0 +1,14 @@
+﻿using CarcassTwwo.Models.Places;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarcassTwwo.Models.Requests.Handlers
+{
+    public class MonasteryHandle : AbstractHandler
+    {
+        public HashSet<Monastery> Monasteries { get; private set; }
+        
+    }
+}
