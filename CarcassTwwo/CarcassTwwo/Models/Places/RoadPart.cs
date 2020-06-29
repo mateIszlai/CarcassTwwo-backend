b89291a8d27985d660a5a18144a70867e12702ac
Mate Iszlai <iszimate@gmail.com>
2020-06-29 19:16:00 +0200

    Remove Lands property

--- CarcassTwwo/CarcassTwwo/Models/Places/RoadPart.cs
+++ CarcassTwwo/CarcassTwwo/Models/Places/RoadPart.cs
@@ -1,19 +1,15 @@
-﻿using System.Collections.Generic;
-
-namespace CarcassTwwo.Models.Places
+﻿namespace CarcassTwwo.Models.Places
 {
     public class RoadPart
     {
         public int CardId { get; private set; }
         public bool LeftOpen { get; set; }
         public bool RightOpen { get; set; }
-        public List<int> Lands { get; set; }
 
         public RoadPart(int cardId)
         {
             CardId = cardId;
             LeftOpen = RightOpen = true;
-            Lands = new List<int>();
         }
     }
 }
